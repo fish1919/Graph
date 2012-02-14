fca2635f5e28fec8136e88f668ad768b51c47635
fish <fish1919@21cn.com>
2012-02-14 19:17:32 +0800

    first version 2012-2-14

---
+++ Graph/Graph/GraphTop.cs
@@ -0,0 +1,14 @@
+﻿//代码来源于http://www.cnblogs.com/abatei/archive/2008/06/06/1215114.html
+//创建图的邻接表
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graph
+{
+    class GraphTop
+    {
+    }
+}
---
+++ Graph/Graph/Vertex.cs
@@ -0,0 +1,33 @@
+﻿//代码来源于http://www.cnblogs.com/abatei/archive/2008/06/06/1215114.html
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graph
+{
+    //嵌套类，表示链表中的表结点
+    public class Node
+    {
+        public Vertex adjvex; //邻接点域
+        public Node next; //下一个邻接点指针域
+        public Node(Vertex value)
+        {
+            adjvex = value;
+        }
+    }
+
+    //表示存放于数组中的表头结点
+    class Vertex
+    {
+        public string data; //数据
+        public Node firstEdge; //邻接点链表头指针
+        public Boolean visited; //访问标志,遍历时使用
+        public Vertex(string value) //构造方法
+        {
+            data = value;
+        }
+    }
+    
+}
